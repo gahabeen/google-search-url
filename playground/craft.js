dde96ad5087f6321d65a0c74ef7bdee045b80f1b
Gabin Desserprit <desserprit.gabin@gmail.com>
2022-01-27 17:52:31 +0100

    Added isValidLocation method

--- playground/craft.js
+++ playground/craft.js
@@ -1,13 +1,15 @@
 const gsearch = require('../index');
 
-console.time('craft')
-gsearch.craft(
-    {
-        location: 'Florida,New York,United States',
-        query: ['site:linkedin.com', '"CEO"', 'bank'],
-        num: 100,
-        page: 1,
-        shuffle: true,
-    },
+console.log(
+
+    gsearch.craft(
+        {
+            location: 'Florida,New York,United States',
+            query: ['site:linkedin.com', '"CEO"', 'bank'],
+            num: 100,
+            page: 1,
+            shuffle: true,
+            sign: true
+        }
+    )
 )
-console.timeEnd('craft')
